609221a657e3f6cb778d1afcac5911be7b2013bb
Vincent Arnaud <varnaud@live.c>
2017-06-03 20:00:27 -0700

    gg

--- graph.c
+++ graph.c
@@ -6,14 +6,15 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 21:05:14 by varnaud           #+#    #+#             */
-/*   Updated: 2017/06/02 23:35:27 by varnaud          ###   ########.fr       */
+/*   Updated: 2017/06/03 19:59:59 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 typedef struct		s_adj_node
 {
 	int					dest;
-	void				*data;
 	struct s_adj_node	*next;
 }					t_adj_node;
 
@@ -28,7 +29,7 @@ typedef struct		s_graph
 	t_adj_list		*array;
 }					t_graph;
 
-t_adj_node	*new_adj_node(int dest, void *data, int size)
+t_adj_node	*new_adj_node(int dest)
 {
 	t_adj_node *node;
 
@@ -36,8 +37,6 @@ t_adj_node	*new_adj_node(int dest, void *data, int size)
 	if (node == NULL)
 		return (NULL);
 	node->dest = dest;
-	node->data = data;
-	node->size = size;
 	node->next = NULL;
 	return (node);
 }
@@ -58,5 +57,48 @@ t_graph		*create_graph(int v)
 
 void		add_edge(t_graph *graph, int src, int dest)
 {
-	
+	t_adj_node	*node;
+
+	node = new_adj_node(dest);
+	node->next = graph->array[src].head;
+	graph->array[src].head = node;
+	node = new_adj_node(src);
+	node->next = graph->array[dest].head;
+	graph->array[dest].head = node;
+}
+
+void		print_graph(t_graph *graph)
+{
+	int			v;
+	t_adj_node	*node;
+
+	v = 0;
+	while (v < graph->v)
+	{
+		node = graph->array[v].head;
+		ft_printf("Adjacency list of vertex %d\n head ", v);
+		while (node)
+		{
+			ft_printf("-> %d", node->dest);
+			node = node->next;
+		}
+		ft_printf("\n");
+		v++;
+	}
+}
+
+int		main(void)
+{
+	int		v = 5;
+	t_graph	*graph;
+
+	graph = create_graph(v);
+	add_edge(graph, 0, 1);
+	add_edge(graph, 0, 4);
+	add_edge(graph, 1, 2);
+	add_edge(graph, 1, 3);
+	add_edge(graph, 1, 4);
+	add_edge(graph, 2, 3);
+	add_edge(graph, 3, 4);
+	print_graph(graph);
 }
